f23572acfb0f00d3f2e6dd9870bfb7b9fa81766f
Filip Banaszczyk <255559@edu.p.lodz.pl>
2026-04-09 15:39:32 +0200

    18 dodanie szkieletów do każdej klasy

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,11 +1,23 @@
 package edu.kis.vh.nursery;
 
 // Projekt jest poprawny. Dokonano walidacji testami jednostkowymi - nie stwierdzono potrzeby wprowadzania zmian.
-
+/**
+ * Klasa FIFORhymer rozszerza DefaultCountingOutRhymer, zmieniając mechanizm
+ * wydobywania elementów na model FIFO (First In, First Out).
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /** * Pomocniczy obiekt rymowanki używany do tymczasowego
+     * przechowywania elementów podczas operacji countOut.
+     */
     private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Zwraca pierwszy element dodany do rymowanki (zgodnie z zasadą kolejki).
+     * Mechanizm polega na przepisaniu wszystkich elementów do obiektu tymczasowego,
+     * pobraniu ostatniego z nich, a następnie przywróceniu pozostałych do głównego stosu.
+     * * @return wartość pierwszego dodanego elementu lub -1 (ERROR), jeśli rymowanka jest pusta.
+     */
     @Override
     public int countOut() {
         while (!callCheck())
@@ -19,6 +31,10 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
         return ret;
     }
 
+    /**
+     * Zwraca obiekt pomocniczy wykorzystywany w tej klasie.
+     * * @return instancja DefaultCountingOutRhymer służąca za bufor tymczasowy.
+     */
     public DefaultCountingOutRhymer getTemp() {
         return temp;
     }
